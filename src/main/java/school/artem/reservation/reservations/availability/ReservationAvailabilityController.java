4f0de53cc0f4e8aa22e53a49aeb8e36eecfec3d1
ArtemChernousovSet <202300991@estudantes.ips.pt>
2026-08-25 18:08:29 +0100

    Clean up reservation code

--- src/main/java/school/artem/reservation/reservations/availability/ReservationAvailabilityController.java
+++ src/main/java/school/artem/reservation/reservations/availability/ReservationAvailabilityController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class ReservationAvailabilityController {
 
     @PostMapping("/check")
     public ResponseEntity<CheckAvailabilityResponse> checkAvailability(
-            @Valid CheckAvailabilityRequest request
+            @RequestBody @Valid CheckAvailabilityRequest request
     ) {
         log.info("Called checkAvailability: request={}", request);
 
--- src/main/java/school/artem/reservation/web/ErrorResponseDto.java
+++ src/main/java/school/artem/reservation/web/ErrorResponseDto.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public record ErrorResponseDto(
         String message,
-        String detailedMessage, // errorMessage
+        String detailedMessage,
         LocalDateTime errorTime
 ) {
 
